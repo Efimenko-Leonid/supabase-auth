385b45fbedc27a65addd16edab0a717c047252e9
Efimenko Leonid <synerg@mail.ru>
2022-09-02 10:46:26 +0800

    upl

---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2>Welcome!</h2>
+      <code className="highlight">You are logged out</code>
+    </Layout>
+  );
+}
