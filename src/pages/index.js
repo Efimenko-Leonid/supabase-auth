ee638a62d2685e60430afc2d398334274f55f5a2
Efimenko Leonid <synerg@mail.ru>
2022-09-02 12:39:30 +0800

    upl

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
 import Layout from '@/components/Layout';
+import { useAuth } from '@/lib/auth';
+import { Auth } from '@supabase/ui';
+import { supabase } from '@/lib/client';
 
 export default function Home() {
+  const { user, signOut } = useAuth();
+
   return (
     <Layout>
-      <h2>Welcome!</h2>
-      <code className="highlight">You are logged out</code>
+      {!user && <Auth supabaseClient={supabase} />}
+      {user && (
+        <>
+          <h2>Welcome!</h2>
+          <code className="highlight">{user.role}</code>
+          <Link href="/profile">
+            <a className="button">Go to Profile</a>
+          </Link>
+          <button type="button" className="button-inverse" onClick={signOut}>
+            Sign Out
+          </button>
+        </>
+      )}
     </Layout>
   );
 }
