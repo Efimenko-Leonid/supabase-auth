d8cc67338234d282e6760cbeb03a666f88c32675
Leonid Efimenko <synerg@mail.ru>
2022-09-07 20:58:28 +0800

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,24 @@ import { supabase } from '@/lib/client';
 export default function Home() {
   const { user, view, signOut } = useAuth();
 
+  // if (view === VIEWS.UPDATE_PASSWORD) {
+  //   return (
+  //     <Layout>
+  //       <Auth.UpdatePassword supabaseClient={supabase} />
+  //     </Layout>
+  //   );
+  // }
+
   return (
     <Layout>
       {user && (
         <>
           <h2>Welcome!</h2>
           <code className="highlight">{user.role}</code>
-          <Link href="/profile">
+          <button href="/profile">
             <a className="button">Go to Profile</a>
-          </Link>
+          </button>
+
           <button type="button" className="button-inverse" onClick={signOut}>
             Sign Out
           </button>
